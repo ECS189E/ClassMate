16d4cf4f9dda2ddbc9bc2a851b2e513b688696e4
Min Duan <806866300@qq.com>
2018-11-19 17:03:52 -0800

    chatSDK setup
    Setting up chatSDK

---
+++ ClassMate/ClassMate/ClassMate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClassMate
+//
+//  Created by Min Duan on 11/19/18.
+//  Copyright © 2018 Sponsor Me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
